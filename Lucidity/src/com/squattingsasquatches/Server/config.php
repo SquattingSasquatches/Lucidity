7602065d6db2dbe9dc0ffb777cc3f2d1559cf1f2
Asa Rudick <Shabonkerz@gmail.com>
2011-10-26 18:11:14 -0500

    Added user registration.

---
+++ Lucidity/src/com/squattingsasquatches/Server/config.php
@@ -0,0 +1,6 @@
+<?php
+	$config['DB_USER'] = 'root';
+	$config['DB_PASS'] = 'password';
+	$config['DB_HOST'] = 'localhost';
+	$config['DB_NAME'] = 'lucidity';
+?>
---
+++ Lucidity/src/com/squattingsasquatches/Server/register.php
@@ -0,0 +1,81 @@
+<?php
+	
+	include('init.php');
+	
+	global $db;
+	
+	extract( $_REQUEST );
+	
+	// If no name parameter is specified, return with error code 1.
+	
+	if( !isset( $name ) ) 
+	{
+		echo '1';
+		return;
+	}
+	
+	if( !isset( $device_id ) ) 
+	{
+		echo '2';
+		return;
+	}
+	
+	$db->select('name','users', 'criteria = ?', array($name) );
+	
+	
+	// If database encounters an error, return with error code 2.
+	
+	if( !$records = $db_fetch_assoc_all() )
+	{
+		echo '3';
+		return;
+	}
+	
+	// Name exists. Return with error code 3.
+	
+	if( !empty( $records ) )
+	{
+		echo '4';
+		return;
+	}
+	
+	$db->select('device_id','user_devices', 'criteria = ?', array($device_id) );
+	
+	
+	// If database encounters an error, return with error code 4.
+	
+	if( !$records = $db_fetch_assoc_all() )
+	{
+		echo '4';
+		return;
+	}
+	
+	// UUID exists. Return with error code 5.
+	
+	if( !empty( $records ) )
+	{
+		echo '5';
+		return;
+	}
+	
+	if ( !$db->insert('name', array( 'name' => $name ) )
+	{
+		echo '6';
+		return;
+	}
+	
+	$user_id = $db->insert_id();
+	
+	if( !$db->insert('user_id, device_id', array( 'user_id' => $user_id, 'device_id' => device_id ) )
+	{	
+		echo '7';
+		return;
+	}
+	
+	
+	$db->show_debug_console();
+	
+	$db->close();
+	
+	echo '0';
+?>
