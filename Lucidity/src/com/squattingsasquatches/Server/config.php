baaf402433b2c03816788305cab1ef81ebf7f10e
wbaaron <wbaaron@crimson.ua.edu>
2011-10-26 18:33:56 -0500

    updated db login info

--- Lucidity/src/com/squattingsasquatches/Server/config.php
+++ Lucidity/src/com/squattingsasquatches/Server/config.php
@@ -1,6 +1,6 @@
 <?php
-	$config['DB_USER'] = 'thesouth_ss';
-	$config['DB_PASS'] = 's}0}Q8h1Uur=';
+	$config['DB_USER'] = 'thesouth_lucuser';
+	$config['DB_PASS'] = 'zkB0o.H0;S5r';
 	$config['DB_HOST'] = '207.210.251.2';
 	$config['DB_NAME'] = 'thesouth_lucidity';
 ?>
