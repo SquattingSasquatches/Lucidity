5caf143c6bf8d3ab907b0050ae874260fc0eaf7c
Asa Rudick <Shabonkerz@gmail.com>
2011-11-27 10:55:47 -0600

    Changing older stuff into the newer multi-output form.

--- Lucidity/src/com/squattingsasquatches/lucidity/Server/user.course.register.php
+++ Lucidity/src/com/squattingsasquatches/lucidity/Server/user.course.register.php
@@ -11,20 +11,24 @@
  
 include('class.controller.php');
 
-class AddCourse extends Controller
+class RegisterCourse extends Controller
 {
 	function execute()
 	{
-	 	$db->insert('courses', array('course_name' => $this->params['course_name'], 'course_department_prefix' => $this->params['course_department_prefix'],'uni_id' => $this->params['uni_id'], 'start_date' => $this->params['start_date'],'end_date' => $this->params['end_date']     ) );
+		$this->db->query('SELECT * FROM `user_devices` AS ud WHERE ud.device_id = ?', array('device_id' => $this->params['device_id'] ));
+	 	
+	 	$records = $db->fetch_assoc_all();
+	 	
+	 	$this->response->addData( $records );
+	 	
+		$this->db->insert('student_courses', array('student_id' => $records['user_id'], 'course_id' => $this->params['course_id'], 'verified' => '0') );
 		
-		$db->show_debug_console();
+		$this->db->show_debug_console();
 		
-		$db->close();
-		
-		$response->send();
+		$this->db->close();
 	
 	}
-	function isNotAlreadyAdded( $param_names )
+	function isNotAlreadyRegistered( $param_names )
 	{
 	 	$this->db->query('SELECT * FROM `users_devices` AS ud, `student_courses` AS sc, WHERE ud.device_id = ? AND ud.user_id = sc.student_id AND ?', array('device_id' => $this->params[$param_names[0]], 'course_id' => $this->params[$param_names[1]] ));
 	 	
@@ -32,13 +36,12 @@ class AddCourse extends Controller
 		
 	 	return false;
 	}
-	function isProfessor( $param_names )
+	function userExists( $param_names )
 	{
-		$this->db->query('SELECT * FROM `user_devices` AS ud, `professors` AS p WHERE ud.device_id = ? AND p.user_id = ud.user_id', array('device_id' => $this->params[$param_names[0]] ));
- 	
-		if( !$this->db->found_rows ) return false;
-		
-		return true;
+		$this->db->query('SELECT * FROM `user_devices` AS ud WHERE ud.device_id = ?', array('device_id' => $this->params[$param_names[0]] ));
+	 	
+	 	if( !$records = $db->fetch_assoc_all() ) return false;
+	 	return true;
 	}
 	function showView()
 	{
@@ -60,13 +63,12 @@ class AddCourse extends Controller
 
 /* Main */
 
-$controller = new AddCourse();
+$controller = new RegisterCourse();
 
 $controller->addValidation( 'device_id', 'isParamSet', 'no_device_id_supplied', true );
 $controller->addValidation( 'course_id', 'isParamSet', 'no_course_id_supplied', true );
-$controller->addValidation( 'course_id', 'isNotAlreadyAdded', 'course_already_registered', true );
+$controller->addValidation( 'course_id', 'isNotAlreadyRegistered', 'course_already_registered', true );
 $controller->addValidation( 'device_id', 'userExists', 'no_user_id_found', true );
-$controller->addValidation( array( 'device_id', 'course_id' ), 'isProfessorOfCourse', 'user_not_professor_of_course', true );
 
 if( $controller->validate() ) $controller->execute();
 
--- Lucidity/src/com/squattingsasquatches/lucidity/Server/user.course.verify.php
+++ Lucidity/src/com/squattingsasquatches/lucidity/Server/user.course.verify.php
@@ -8,58 +8,68 @@
  * Parameters: device_id, course_id, student_id
  * 
  */
- 	
- 	include('init.php');
-	
-	global $db;
-	$db->debug = true;
-	extract( $_REQUEST );
- 
- 	
- 	/*
- 	 * 		Parameter checks.
- 	 */
- 	
- 	if( !isset( $device_id ) )
- 	{
- 		$response->addError('no_device_id_supplied', true);
- 	}
- 	
-	if( !isset( $course_id ) )
- 	{
- 		$response->addError('no_course_id_supplied', true);
- 	}
-	
-	if( !isset( $student_id ) )
- 	{
- 		$response->addError('no_student_id_supplied', true);
- 	}
- 	
- 	
- 	
- 	
-	
-	$db->query('SELECT * FROM `user_devices` AS ud, `professors` AS p, `courses` AS c, `professor_courses` AS pc WHERE ud.device_id = ? AND p.user_id = ud.user_id AND p.user_id = pc.professor_id AND pc.course_id = c.id  AND c.id = ?', array('device_id' => $device_id, 'course_id' => $course_id ));
- 	
-	if( !$db->found_rows )
+
+include('class.controller.php');
+
+class VerifyCourse extends Controller
+{
+	function execute()
 	{
-		// No professor id found.
+	 	$db->update('student_courses', array('is_verified' => '1'), 'course_id = ? AND student_id = ?', array($this->params['course_id'], $this->params['student_id']) );
+		
 		$db->show_debug_console();
- 		$response->addError('user_not_professor_of_course', true);
+		
+		$db->close();
+		
+		$response->send();
+	
+	}
+	function isNotAlreadyAdded( $param_names )
+	{
+	 	$this->db->query('SELECT * FROM `users_devices` AS ud, `student_courses` AS sc, WHERE ud.device_id = ? AND ud.user_id = sc.student_id AND ?', array('device_id' => $this->params[$param_names[0]], 'course_id' => $this->params[$param_names[1]] ));
+	 	
+	 	if( $this->db->found_rows ) return true;
+		
+	 	return false;
+	}
+	function isProfessorOfCourse( $param_names )
+	{
+		$this->db->query('SELECT * FROM `user_devices` AS ud, `professors` AS p, `courses` AS c, `professor_courses` AS pc WHERE ud.device_id = ? AND p.user_id = ud.user_id AND p.user_id = pc.professor_id AND pc.course_id = c.id  AND c.id = ?', array('device_id' => $this->params[$param_names[0]], 'course_id' => $this->params[$param_names[1]] ));
+ 	
+		if( !$this->db->found_rows ) return false;
+		
+		return true;
+	}
+	function showView()
+	{
+		$this->db->query('SELECT * FROM `courses`');
+ 	
+	 	$records = $this->db->fetch_assoc_all();
+	 	
+	 	$this->response->addData( $records );
+	 	$this->response->addData( $this->params );
+	 	
+		$this->db->close();
+		
+		$this->response->send();
+		
 	}
 	
-	$db->update('student_courses', array('is_verified' => '1'), 'course_id = ? AND student_id = ?', array($course_id, $student_id) );
-	
-	if( !$db->affected_rows )
-	{
-		// No course id found.
-		$db->show_debug_console();
- 		$response->addError('no_student_course_pair_found', true);
-	}
-	
-	$db->show_debug_console();
-	
-	$db->close();
-	
-	$response->send();
+}
+
+
+/* Main */
+
+$controller = new AddCourse();
+
+$controller->addValidation( 'device_id', 'isParamSet', 'no_device_id_supplied', true );
+$controller->addValidation( 'course_id', 'isParamSet', 'no_course_id_supplied', true );
+$controller->addValidation( 'student_id', 'isParamSet', 'no_student_id_supplied', true );
+$controller->addValidation( 'device_id', 'userExists', 'no_user_id_found', true );
+$controller->addValidation( array( 'device_id', 'course_id' ), 'isProfessorOfCourse', 'user_not_professor_of_course', true );
+
+if( $controller->validate() ) $controller->execute();
+
+$controller->showView();
+
 ?>
