6ee58a02656de891c9a746928ee0de9aba7bc476
Asa Rudick <Shabonkerz@gmail.com>
2011-12-01 23:50:11 -0600

    Trying to get thigns back on track...

--- Lucidity/src/com/squattingsasquatches/lucidity/CourseDescriptionActivity.java
+++
@@ -1,7 +0,0 @@
-package com.squattingsasquatches.lucidity;
-
-import android.app.Activity;
-
-public class CourseDescriptionActivity extends Activity {
-
-}
--- Lucidity/src/com/squattingsasquatches/lucidity/Server/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Created on Nov 23, 2011
- *
- * Lucidity 
- * Created by Asa Rudick, Brett Aaron, Trypp Cook
- */
- phpinfo();
-?>
