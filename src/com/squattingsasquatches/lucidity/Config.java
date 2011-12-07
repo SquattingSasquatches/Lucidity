96fe4734b473a8eab173cf458c76f0578dae4937
htryppcook <htcook@crimson.ua.edu>
2011-12-06 20:26:21 -0500

    manually updating for ability to change server IP on the fly

--- src/com/squattingsasquatches/lucidity/Config.java
+++ src/com/squattingsasquatches/lucidity/Config.java
@@ -1,6 +1,10 @@
 package com.squattingsasquatches.lucidity;
 
 public final class Config {
-	public static final String SERVER_ADDRESS = "http://192.168.1.5";
+	public static String SERVER_ADDRESS = "http://130.160.211.88:8080";
 	public static final String USER_AGENT = "Lucidity/android";
+	
+	public static void setServerAddress(String ADDRESS){
+		SERVER_ADDRESS = ADDRESS;
+	}
 }
